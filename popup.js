684da7ac3901e80f74846ee0ae25ff26cd5fee1d
Nurbek Yelesh <boolevafunkce@gmail.com>
2022-05-21 23:32:52 +0200

    complete tutorial file replacement

--- popup.js
+++ popup.js
@@ -1,15 +1,24 @@
-let changeColor = document.getElementById('changeColor');
+// Initialize butotn with users's prefered color
+let changeColor = document.getElementById("changeColor");
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
 });
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+// When the button is clicked, inject setPageBackgroundColor into current page
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
   });
-};
+});
+
+// The body of this function will be execuetd as a content script inside the
+// current page
+function setPageBackgroundColor() {
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+}
